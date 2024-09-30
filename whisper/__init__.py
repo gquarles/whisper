88a8be957ef6c68b1d9a11c7583ec0f044389319
Griffin Quarles <griffinq178@gmail.com>
2024-09-29 20:00:00 -0400

    no checksum

--- whisper/__init__.py
+++ whisper/__init__.py
@@ -32,7 +32,6 @@ _MODELS = {
 def _download(url: str, root: str, in_memory: bool) -> Union[bytes, str]:
     os.makedirs(root, exist_ok=True)
 
-    expected_sha256 = url.split("/")[-2]
     download_target = os.path.join(root, os.path.basename(url))
 
     if os.path.exists(download_target) and not os.path.isfile(download_target):
@@ -41,11 +40,7 @@ def _download(url: str, root: str, in_memory: bool) -> Union[bytes, str]:
     if os.path.isfile(download_target):
         with open(download_target, "rb") as f:
             model_bytes = f.read()
-        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
-            return model_bytes if in_memory else download_target
-        else:
-            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; proceeding anyways")
-            return model_bytes if in_memory else download_target
+        return model_bytes if in_memory else download_target
 
     with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
         with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True,
@@ -59,10 +54,6 @@ def _download(url: str, root: str, in_memory: bool) -> Union[bytes, str]:
                 loop.update(len(buffer))
 
     model_bytes = open(download_target, "rb").read()
-    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
-        raise RuntimeError(
-            "Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model.")
-
     return model_bytes if in_memory else download_target
 
 
